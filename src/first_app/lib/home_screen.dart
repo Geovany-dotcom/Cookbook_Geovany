3a32ac354088af355e0b832eea65576d6932284a
Geovany-dotcom <adriand287@gmail.com>
2025-01-23 00:38:32 -0600

    Updated HomeScreen design and added new features

--- src/first_app/lib/home_screen.dart
+++ src/first_app/lib/home_screen.dart
@@ -22,66 +22,68 @@ class HomeScreen extends StatelessWidget {
             color: Colors.grey[900],
             borderRadius: const BorderRadius.vertical(top: Radius.circular(25.0)),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _buildMenuItem(
-                context,
-                icon: Icons.design_services,
-                color: Colors.blue,
-                title: 'Go to Design Section',
-                screen: DesignScreen(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.list,
-                color: Colors.green,
-                title: 'Go to List Section',
-                screen: ListScreen(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.edit,
-                color: Colors.orange,
-                title: 'Go to Forms Section',
-                screen: FormsScreen(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.image,
-                color: Colors.purple,
-                title: 'Go to Images Section',
-                screen: ImagesScreen(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.navigation,
-                color: Colors.red,
-                title: 'Go to Navigation Section',
-                screen: NavigationScreen(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.animation,
-                color: Colors.teal,
-                title: 'Go to Animation Section',
-                screen: AnimationHomePage(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.network_cell,
-                color: Colors.blueGrey,
-                title: 'Go to Network Section',
-                screen: NetworkHomePage(),
-              ),
-              _buildMenuItem(
-                context,
-                icon: Icons.storage,
-                color: Colors.deepOrange,
-                title: 'Go to Local Storage Section',
-                screen: LocalStoreScreen(),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildMenuItem(
+                  context,
+                  icon: Icons.design_services,
+                  color: Colors.blue,
+                  title: 'Go to Design Section',
+                  screen: DesignScreen(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.list,
+                  color: Colors.green,
+                  title: 'Go to List Section',
+                  screen: ListScreen(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.edit,
+                  color: Colors.orange,
+                  title: 'Go to Forms Section',
+                  screen: FormsScreen(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.image,
+                  color: Colors.purple,
+                  title: 'Go to Images Section',
+                  screen: ImagesScreen(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.navigation,
+                  color: Colors.red,
+                  title: 'Go to Navigation Section',
+                  screen: NavigationScreen(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.animation,
+                  color: Colors.teal,
+                  title: 'Go to Animation Section',
+                  screen: AnimationHomePage(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.network_cell,
+                  color: Colors.blueGrey,
+                  title: 'Go to Network Section',
+                  screen: NetworkHomePage(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.storage,
+                  color: Colors.deepOrange,
+                  title: 'Go to Local Storage Section',
+                  screen: LocalStoreScreen(),
+                ),
+              ],
+            ),
           ),
         );
       },
@@ -126,42 +128,45 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Bienvenido a la pochocloApp',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Bienvenido a la pochocloApp',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                  shadowColor: Colors.black,
-                  elevation: 8,
+                  textAlign: TextAlign.center,
                 ),
-                onPressed: () => _showMenu(context),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Icon(Icons.menu, color: Colors.white, size: 24),
-                    SizedBox(width: 12),
-                    Text(
-                      'Open Menu',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  ],
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                    shadowColor: Colors.black,
+                    elevation: 8,
+                  ),
+                  onPressed: () => _showMenu(context),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.menu, color: Colors.white, size: 24),
+                      SizedBox(width: 12),
+                      Text(
+                        'Open Menu',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
